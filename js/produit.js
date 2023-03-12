c6d5f87b9325c5f27ca77ea6d7e4583ba62f6325
Valentin Serres <108737662+Reloul@users.noreply.github.com>
2023-03-12 17:31:49 +0100

    Add files via upload

--- js/produit.js
+++ js/produit.js
@@ -33,8 +33,45 @@ function affStock(para){
     var id = document.getElementById(para);
     if(id.style.visibility === "hidden"){
         id.style.visibility = "visible";
+        id.style.width = "20px";
     }
     else{
         id.style.visibility ="hidden";
+        id.style.width = "0";
+    }
+}
+
+function moins(para, m, p){
+    var id = document.getElementById(para);
+    var boutonM = document.getElementById(m);
+    var boutonP = document.getElementById(p);
+    if (id.value != 0){
+        id.value = id.value-1;
+        if(boutonP.style.visibility === "hidden"){
+            boutonP.style.visibility = "visible";
+        }
+        if(id.value == 0){
+            boutonM.style.visibility= "hidden";
+        }
+    }else{
+        boutonM.style.visibility= "hidden";
+    }
+}
+
+function plus(para, max, m, p){
+    var quant = document.getElementById(max).textContent;
+    var id = document.getElementById(para);
+    var boutonM = document.getElementById(m);
+    var boutonP = document.getElementById(p);
+    if (parseInt(id.value) < parseInt(quant)){
+        id.value = parseInt(id.value)+1;
+        if (boutonM.style.visibility === "hidden"){
+            boutonM.style.visibility = "visible";
+        }
+        if(id.value == parseInt(quant)){
+            boutonP.style.visibility= "hidden";
+        }
+    }else{
+        boutonP.style.visibility = "hidden";
     }
 }
