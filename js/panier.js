316f47020522a11519fed88c3a783c53a002e94c
Valentin Serres <108737662+Reloul@users.noreply.github.com>
2023-04-15 16:59:17 +0200

    Add files via upload

--- js/panier.js
+++ js/panier.js
@@ -15,7 +15,7 @@ function ajouterPanier(idImg, quant, e) {
     ligne.setAttribute('id', nom);
 
     var bouton = document.createElement('button');
-    bouton.setAttribute('id', bouton + nom);
+    bouton.setAttribute('id', 'bouton' + nom);
     bouton.setAttribute('onclick', "supprimerPanier('" + nom + quantite + "' , '" + (quantite * prix) + "')");
     
     var divGlob = document.createElement('div');
@@ -34,25 +34,45 @@ function ajouterPanier(idImg, quant, e) {
 
     newDiv.setAttribute('id', nom + quantite);
     // Créer un nouveau span pour le nom de l'article
-    var newSpan = document.createElement('span');
-    newSpan.textContent = nom;
+    var nomLabel = document.createElement('label');
+    nomLabel.textContent = nom;
+    var nomInput = document.createElement('input');
+    nomInput.type = 'text';
+    nomInput.value = nom;
+    nomInput.name = "nom[]";
     
     // Créer un nouvel élément img pour l'image de l'article
     var newImg = document.createElement('img');
     newImg.src = document.getElementById(idImg).src;
-    
+    var imgInput = document.createElement('input');
+    imgInput.type='text';
+    imgInput.value= newImg.src
+    imgInput.name="img[]";  
     // Créer un nouveau span pour le prix et la quantité
-    var newDetails = document.createElement('span');
-    newDetails.textContent = prix + '€ - Quantité : ' + quantite;
-    
+    var prixQLabel = document.createElement('label');
+    prixQLabel.textContent = prix + '€ - Quantité : ' + quantite;
+    var prixInput = document.createElement('input');
+    var quantInput = document.createElement('input');
+
+    prixInput.type = 'text';
+    prixInput.value = prix;
+    prixInput.name = "prix[]";
+
+    quantInput.type = 'text';
+    quantInput.value = quantite;
+    quantInput.name = "quant[]";
     // Ajouter les éléments à la div "panier"
     var panier = document.querySelector('.produitV');
 
     var cout = document.getElementById('prix');
     var tot = parseFloat(document.getElementById('prix').textContent, 10);
     divGlob.appendChild(newImg);
-    groupSpan.appendChild(newSpan);
-    groupSpan.appendChild(newDetails);
+    divGlob.appendChild(imgInput);
+    groupSpan.appendChild(nomLabel);
+    groupSpan.appendChild(nomInput);
+    groupSpan.appendChild(prixQLabel);
+    groupSpan.appendChild(prixInput);
+    groupSpan.appendChild(quantInput);
     divGlob.appendChild(groupSpan);
     bouton.appendChild(icon);
     divGlob.appendChild(bouton);
