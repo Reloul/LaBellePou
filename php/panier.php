a18e7b3d55895c42df801237ba7b6c5f249eb5c5
Valentin Serres <108737662+Reloul@users.noreply.github.com>
2023-04-15 16:58:58 +0200

    Add files via upload

--- php/panier.php
+++ php/panier.php
@@ -1,5 +1,6 @@
 <?php
      session_start();
+     $email = $_SESSION['email'];
 ?>
 <div class="toutPanier">
      <nav class="panier">
@@ -7,11 +8,39 @@
                <h3>Voici votre Panier : </h3>
           </div>
           <form class="produitV" method="post" action="info.php">
-               <input type="submit" value="Sauvegarder Panier">
+               <input type="submit" value="Sauvegarder Panier" id="sauve">
                <hr>
+               <?php
+                    $json = file_get_contents('../json/info.json');
+                    $tab = json_decode($json, true);
+                    foreach($tab[$email] as $personne){
+                         echo "<div id='".$personne['nom'].$personne['quantite']."' class='article'>";
+                         echo "<div class='global'>";
+                         echo "<img src='".$personne['srcImg']."'>";
+                         echo "<input type='text' name='img[]' value='".$personne['srcImg']."'>";
+                         echo "<div class='spanG'>";
+                         echo "<label>".$personne['nom']."</label>";
+                         echo "<input type='text' name='nom[]' value='".$personne['nom']."'>";
+                         echo "<label>".$personne['prix']."€ - Quantité : ".$personne['quantite']."</label>";
+                         echo "<input type='text' name='prix[]' value='".$personne['prix']."'>";
+                         echo "<input type='text' name='quant[]' value='".$personne['quantite']."'>";
+                         echo "</div>";
+                         echo "<button id='button".$personne['nom']."' onclick=\"supprimerPanier('".$personne['nom'].$personne['quantite']."', '".$personne['quantite']."')\">";
+                         echo "<ion-icon name='close-outline'></ion-icon>";
+                         echo "</button></div>";
+                         echo "<hr id=".$personne['nom'].">";
+                         echo "<div>";
+                    }
+               ?>
           </form>
           <div class="sousTotal">
-               <h3>Sous-Total : <span id="prix">0€</span></h3>
+               <?php
+                    $somme = 0;
+                    foreach($tab[$email] as $personne){
+                         $somme = $personne['prix']*$personne['quantite'] + $somme;
+                    }
+               ?>
+               <h3>Sous-Total : <span id="prix"><?php echo $somme ?>€</span></h3>
           </div>
      </nav>
      <div class="toggleee">
