e0967e23ee8e7a7ea1c32eea17f18342d612498c
Ugo Latry <83210143+Ugolty@users.noreply.github.com>
2023-05-17 23:41:32 +0200

    Update majStock.php

--- php/majStock.php
+++ php/majStock.php
@@ -1,24 +1,20 @@
 <?php
-$productId = $_POST['productId'];
-$quantity = $_POST['quantity'];
-$conn = new mysqli('localhost', 'latryugo', 'guillaumelepd', 'Produits');
+require_once("bddDATA.php");
+function Connexion(){
+  global $basedd;
 
-// Vérifier si la connexion à la base de données a réussi
-if ($conn->connect_error) {
-  die('Erreur de connexion à la base de données : ' . $conn->connect_error);
+  $basedd = mysqli_connect(SERV, USER, PSWRD, BASE);
+
+  if(!$basedd){
+      throw new Exception(mysqli_connect_error());
+  }
+
+  return true;
 }
 
-// Mettre à jour la quantité dans la base de données
-$query = 'UPDATE Poubelle SET quantite = quantite + $quantity WHERE idPoubelle = $productId';
-$result = $conn->query($query);
+$id = $_GET['id'];
+$quantite = $_GET['quantite'];
 
-// Vérifier si la mise à jour a réussi
-if ($result) {
-  echo 'Quantité mise à jour avec succès.';
-} else {
-  echo 'Erreur lors de la mise à jour de la quantité : ' . $conn->error;
-}
-
-// Fermer la connexion à la base de données
-$conn->close();
+$req = "UPDATE Produit SET quantite = $quantite WHERE id = $id";
+$result = mysqli_query($basedd, $req);
 ?>
